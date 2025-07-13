ce4262c62c909b2f0e7fc2f2274973c9a4041985
rgeron <raphaelgeron123@gmail.com>
2025-07-13 10:35:30 +0200

    setup for R2-cloud

---
+++ src/features/r2-bucket/utils/awss3.utils.ts
@@ -0,0 +1,38 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+  region: "auto",
+  endpoint: process.env.AWS_S3_API_URL ?? "",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+  },
+});
+
+export async function uploadFileToS3(params: {
+  file: File;
+  prefix: string;
+  filename: string;
+}) {
+  const filebuffer = await params.file.arrayBuffer();
+  const buffer = Buffer.from(filebuffer);
+  const fileExtension = params.filename.split(".").pop();
+  const uniqueFilename = `${params.prefix}/${params.filename}.${fileExtension}`;
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_S3_BUCKET_NAME ?? "",
+    Key: uniqueFilename,
+    Body: buffer,
+    ContentType: params.file.type,
+  });
+
+  try {
+    await s3Client.send(command);
+
+    return {
+      url: `${process.env.R2_URL}/${uniqueFilename}`,
+    };
+  } catch (error) {
+    throw error;
+  }
+}
